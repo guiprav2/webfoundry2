7d163baf57d08596ff5451037af2abe72929b3b6
Gui Prá <gui@guiprav.com>
2024-01-07 16:43:30 -0300

    .

---
+++ App.js
@@ -0,0 +1,320 @@
+import JSZip from 'https://cdn.skypack.dev/jszip';
+import d from './dominant.js';
+import lf from 'https://cdn.skypack.dev/localforage';
+import structuredFiles from './structuredFiles.js';
+import { nanoid } from 'https://cdn.skypack.dev/nanoid';
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js', { type: 'module' }).then(function(registration) {
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }).catch(function(error) {
+    console.log('ServiceWorker registration failed: ', error);
+  });
+}
+
+class App {
+  constructor() {
+    window.app = this;
+    this.loadSites();
+  }
+
+  render = () => d.jsx`
+    <div class="w-80 h-screen shrink-0 flex flex-col bg-[#2b2d31] text-[#949ba4] shadow-2xl">
+      <div class="border-b border-[#1f2124] px-5 py-3">
+        <div class="gfont-[Pacifico] text-gray-100">Webfoundry</div>
+      </div>
+      <div class="flex gap-3 justify-around border-b border-[#1f2124] p-3 text-sm">
+        ${this.menuBtn('Sites', 'sites')}
+        ${this.menuBtn('Files', 'files')}
+        ${this.menuBtn('Styles', 'styles')}
+      </div>
+      ${d.portal(() => this.sidebar)}
+    </div>
+    ${this.iframe = d.jsx`<iframe class="flex-1">`}
+  `;
+
+  menuBtn = (label, key) => d.jsx`
+    <button ${{
+      class: () => this.activeSidebar === key && 'text-gray-100 underline',
+      onClick: () => this.activeSidebar = key,
+    }}>
+      ${label}
+    </button>
+  `;
+
+  sidebars = { sites: d.el(SitesSidebar), files: d.el(FilesSidebar), styles: d.el(StylesSidebar) };
+  activeSidebar = 'sites';
+  get sidebar() { return this.sidebars[this.activeSidebar] }
+
+  sites = {};
+  files = [];
+  styles = [];
+
+  loadSites = () => {
+    this.sites = {};
+    for (let k of Object.keys(localStorage).filter(k => k.startsWith('site:'))) {
+      this.sites[k.slice('site:'.length)] = localStorage.getItem(k);
+    }
+    d.update();
+  };
+
+  openSite = async x => {
+    this.currentSite = x;
+    this.activeSidebar = 'files';
+    await this.loadFiles();
+    d.update();
+  };
+
+  loadFiles = async () => {
+    this.files = [];
+    d.update();
+    let keys = await lf.keys();
+    let prefix = `file:${this.currentSite}:`;
+    let files = keys.filter(x => x.startsWith(prefix)).map(x => x.slice(prefix.length));
+    this.files = structuredFiles(files);
+    d.update();
+  };
+}
+
+class SitesSidebar {
+  onNewSiteKeyDown = ev => {
+    if (ev.key !== 'Enter') { return }
+    let name = ev.target.value.trim();
+    if (!name) { return }
+    ev.target.value = '';
+    localStorage.setItem(`site:${nanoid()}`, name);
+    app.loadSites();
+  };
+
+  render = () => d.jsx`
+    <div class="flex-1 overflow-auto">
+        <div class="flex flex-col gap-1 p-3 text-sm">
+            ${d.map(() => Object.keys(app.sites), x => d.jsx`
+                <a class="flex gap-2 justify-between items-center rounded px-3 py-1" href="#" ${{ onClick: () => app.openSite(x) }}>
+                    <div class="flex gap-2 items-center">
+                        <i class="nf nf-fa-sitemap"></i>
+                        <span>${d.text(() => app.sites[x])}</span>
+                    </div>
+                    <div class="relative top-[-1px] flex gap-2">
+                        <button class="nf nf-fa-pencil"></button>
+                        <button class="nf nf-fa-trash"></button>
+                    </div>
+                </a>
+            `)}
+            <div class="flex gap-2 justify-between items-center rounded px-3 py-1">
+                <div class="flex gap-2 items-center">
+                    <i class="nf nf-fa-plus"></i>
+                    <input class="outline-none bg-transparent" placeholder="new site" ${{ onKeyDown: this.onNewSiteKeyDown }}>
+                </div>
+            </div>
+        </div>
+    </div>
+  `;
+}
+
+class FilesSidebar {
+  openFile = path => {
+    app.iframe.src = `/files/${app.currentSite}/${path}`;
+  };
+
+  newFile = async path => {
+    let [btn, detail] = await showModal(d.el(NewDialog));
+    if (btn !== 'ok') { return }
+    let [type, name] = detail;
+    let prefix = `file:${app.currentSite}:`;
+    if (path) { prefix += `${path}/` }
+    switch (type) {
+      case 'file': await lf.setItem(`${prefix}${name}`, ''); break;
+      case 'folder': await lf.setItem(`${prefix}${name}/.keep`, ''); break;
+    }
+    await app.loadFiles();
+  };
+
+  renameFile = async (path, isDir) => {
+    let [btn, detail] = await showModal(d.el(RenameDialog, { name: path.split('/').pop() }));
+    if (btn !== 'ok') { return }
+    let prefix = `file:${app.currentSite}:`;
+    let newPath = path.split('/').slice(0, -1).join('/');
+    if (newPath) { newPath += '/' }
+    newPath += detail;
+    if (!isDir) {
+      await lf.setItem(`${prefix}${newPath}`, await lf.getItem(`${prefix}${path}`));
+      await lf.removeItem(`${prefix}${path}`);
+    } else {
+      let keys = (await lf.keys()).filter(x => x.startsWith(`${prefix}${path}/`));
+      await Promise.all(keys.map(async x => await lf.setItem(`${prefix}${x.slice(prefix.length).replace(path, newPath)}`, await lf.getItem(x))));
+      await Promise.all(keys.map(x => lf.removeItem(x)));
+    }
+    await app.loadFiles();
+  };
+
+  rmFile = async (path, isDir) => {
+    let prefix = `file:${app.currentSite}:${path}`;
+    if (!isDir) { await lf.removeItem(prefix); await app.loadFiles(); return }
+    let keys = (await lf.keys()).filter(x => x.startsWith(`${prefix}/`));
+    await Promise.all(keys.map(x => lf.removeItem(x)));
+    await app.loadFiles();
+  };
+
+  importZip = async () => {
+    let input = document.createElement('input');
+    input.style.display = 'hidden';
+    input.type = 'file';
+    input.accept = '.zip';
+
+    input.onchange = async ev => {
+      input.remove();
+      let file = ev.target.files[0];
+      if (!file) { return }
+      let zip = await JSZip.loadAsync(file);
+      for (let [path, entry] of Object.entries(zip.files)) {
+        if (path.endsWith('/')) { continue }
+        let blob = await entry.async('blob');
+        await lf.setItem(`file:${app.currentSite}:${path}`, blob);
+      }
+      await app.loadFiles();
+    };
+
+    input.click();
+  };
+
+  render = () => d.jsx`
+    <div class="flex-1 overflow-auto">
+        <div class="flex flex-col gap-1 p-3 text-sm">
+            <div class="flex gap-2 justify-between items-center rounded px-3 py-1">
+                <div class="flex gap-2 items-center">
+                    <i class="nf nf-fa-sitemap"></i>
+                    ${d.text(() => app.sites[app.currentSite])}
+                </div>
+                <div class="relative top-[-1px] flex gap-2">
+                    <button class="nf nf-fa-plus" ${{ onClick: () => this.newFile() }}></button>
+                </div>
+            </div>
+            ${d.map(() => app.files, ([name, path, isDir]) => d.jsx`
+                <a href="#" ${{
+                    class: ['flex gap-2 justify-between items-center rounded px-3 py-1', () => `ml-${(path.split('/').length - 1) * 3}`],
+                    onClick: () => !isDir && this.openFile(path ? `${path}/${name}` : name),
+                }}>
+                    <div class="flex gap-2 items-center">
+                        <i ${{ class: ['nf', () => `nf-fa-${isDir ? 'folder' : 'file'}`] }}></i>
+                        <span>${name}</span>
+                    </div>
+                    <div class="relative top-[-1px] flex gap-2">
+                        ${isDir && d.jsx`<button class="nf nf-fa-plus" ${{ onClick: () => this.newFile(path ? `${path}/${name}` : name) }}></button>`}
+                        <button class="nf nf-fa-pencil" ${{ onClick: () => this.renameFile(path ? `${path}/${name}` : name, isDir) }}></button>
+                        <button class="nf nf-fa-trash" ${{ onClick: () => this.rmFile(path ? `${path}/${name}` : name, isDir) }}></button>
+                    </div>
+                </a>
+            `)}
+            <button class="rounded px-3 py-1 border border-[#949ba4]" ${{ onClick: this.importZip }}>Import ZIP</button>
+        </div>
+    </div>
+  `;
+}
+
+class NewDialog {
+  render = () => this.root = d.jsx`
+    <dialog class="p-0 bg-[#262626] rounded text-white text-sm shadow-xl">
+        <div class="border-b border-neutral-900">
+            <div class="px-3 py-2 flex items-center gap-3">
+                New:
+                ${this.typeSelect = d.jsx`
+                  <select class="bg-transparent">
+                      <option value="file" selected="">File</option>
+                      <option value="folder">Folder</option>
+                  </select>
+                `}
+            </div>
+        </div>
+        <div class="p-3">
+            <div class="flex gap-3 items-center">
+                Name: ${this.nameInput = d.jsx`<input class="w-full bg-[#2b2d31] rounded px-2 py-1 outline-none">`}
+            </div>
+        </div>
+        <div class="border-neutral-900 border-t px-3 py-2 flex gap-2">
+            <button class="px-3 py-1 bg-[#2b2d31] rounded flex-1" ${{ onClick: () => this.root.close('cancel') }}>Cancel</button>
+            <button class="px-3 py-1 rounded flex-1 bg-[#4f46e5]" ${{ onClick: this.ok }}>OK</button>
+        </div>
+    </dialog>
+  `;
+
+  ok = () => {
+    this.root.returnValue2 = [this.typeSelect.value, this.nameInput.value];
+    this.root.close('ok');
+  };
+}
+
+class RenameDialog {
+  constructor(props) { this.props = props }
+  get name() { return this.props.name }
+
+  render = () => this.root = d.jsx`
+    <dialog class="p-0 bg-[#262626] rounded text-white text-sm shadow-xl">
+        <div class="border-b border-neutral-900">
+            <div class="px-3 py-2 flex items-center gap-3">Rename</div>
+        </div>
+        <div class="p-3">
+            <div class="flex gap-3 items-center">
+                ${this.nameInput = d.jsx`<input class="w-full bg-[#2b2d31] rounded px-2 py-1 outline-none" ${{ value: this.name }}>`}
+            </div>
+        </div>
+        <div class="border-neutral-900 border-t px-3 py-2 flex gap-2">
+            <button class="px-3 py-1 bg-[#2b2d31] rounded flex-1" ${{ onClick: () => this.root.close('cancel') }}>Cancel</button>
+            <button class="px-3 py-1 rounded flex-1 bg-[#4f46e5]" ${{ onClick: this.ok }}>OK</button>
+        </div>
+    </dialog>
+  `;
+
+  ok = () => {
+    this.root.returnValue2 = this.nameInput.value;
+    this.root.close('ok');
+  };
+}
+
+function makePromise() {
+  let res, rej, p = new Promise((resolve, reject) => {
+    res = resolve;
+    rej = reject;
+  });
+  return [p, res, rej];
+}
+
+async function showModal(x) {
+  let [p, res] = makePromise();
+  document.body.append(x);
+  x.returnValue = '';
+  x.addEventListener('close', () => {
+    x.remove();
+    res([x.returnValue, x.returnValue2]);
+  });
+  x.showModal();
+  return p;
+}
+
+class StylesSidebar {
+  render = () => d.jsx`
+    <div class="flex-1">
+        <div class="flex flex-col gap-1 p-3 text-sm">
+            ${d.map(() => app.styles, x => d.jsx`
+                <a class="flex gap-2 justify-between items-center rounded px-3 py-1" href="#">
+                <div class="flex gap-2 items-center">
+                    <i class="nf nf-fa-paint_brush"></i>
+                    <span>${x}</span>
+                </div>
+                <div class="relative top-[-1px] flex gap-2">
+                    <button class="nf nf-fa-trash"></button>
+                </div>
+                </a>
+            `)}
+            <div class="flex gap-2 justify-between items-center rounded px-3 py-1">
+                <div class="flex gap-2 items-center">
+                    <i class="nf nf-fa-plus"></i>
+                    <input class="outline-none bg-transparent" placeholder="add class">
+                </div>
+            </div>
+        </div>
+    </div>
+  `;
+}
+
+export default App;
---
+++ sw.js
@@ -0,0 +1,25 @@
+import lf from 'https://cdn.skypack.dev/localforage';
+
+self.addEventListener('fetch', event => {
+  // Assuming the URL pattern to check is something like '/files/yourfile.jpg'
+  if (event.request.url.includes('/files/')) {
+    const url = new URL(event.request.url);
+    const path = url.pathname;
+    
+    // Extract the file name and prepend with 'file:' to match the localForage key
+    const parts = path.slice('/files/'.length).split('/');
+    const site = parts.shift();
+    const fileKey = 'file:' + site + ':' + parts.join('/');
+
+    event.respondWith(lf.getItem(fileKey).then(file => {
+      if (file) {
+        return new Response(file);
+      } else {
+        return new Response('File not found', { status: 404 });
+      }
+    }).catch(error => {
+      return new Response('Error fetching file: ' + error, { status: 500 });
+    })
+    );
+  }
+});
